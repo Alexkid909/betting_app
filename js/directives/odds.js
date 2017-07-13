67c7f04127928fd8a81d9a80ae284a8368c5ed4d
Alex Robinson <alex.robinson578@gmail.com>
2017-07-13 14:44:14 +0100

    refactored odds directive to use isolated scope and link function to calculate odds

--- js/directives/odds.js
+++ js/directives/odds.js
@@ -1,6 +1,22 @@
 app.directive('odds',function() {
 	return {
 		restrict: 'E',
-		templateUrl: 'js/directives/templates/odds.html'
+		templateUrl: 'js/directives/templates/odds.html',
+		scope: {
+			decimalOdds: '=',
+			numerator: '=',
+			denominator: '='
+		},
+		link: function(scope,element,attrs) {
+			debugger;
+			scope.odds = function() {
+				if (scope.decimalOdds) {
+					let decimalOdds = (scope.numerator / scope.denominator) + 1;
+					return parseFloat(Math.round(decimalOdds * 100) / 100).toFixed(2);
+				} else {
+					return scope.numerator+"/"+scope.denominator;
+				};
+			}
+		}
 	}
 });
